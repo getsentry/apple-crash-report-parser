ac98d586ef0ca2d740cd19e7c6f2ce8300e0a098
Armin Ronacher <armin.ronacher@active-4.com>
2019-01-13 01:32:50 +0100

    feat: Added craft config

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! This library implements a simple parser for the apple crash report
 //! text format.
-//! 
+//!
 //! This library also defines a `with_serde` feature to enable serde
 //! serialization (not not deserialization).
 mod parser;
